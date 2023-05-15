b3cfae98fbdedfbf2c9206bcfface17441611301
fajarm <andhikadika7878@gmail.com>
2023-05-15 18:08:57 +0700

    Initial commit

---
+++ routes/SupplierRoute.js
@@ -0,0 +1,18 @@
+// import express from "express";
+// import { 
+//     getSuppliers, 
+//     getSupplierById, 
+//     createSupplier,
+//     updateSupplier,
+//     deleteSupplier
+// } from "../controllers/SupplierController.js";
+
+// const router = express.Router();
+
+// router.get('/suppliers', getSuppliers);
+// router.get('/suppliers/:id', getSupplierById);
+// router.post('/suppliers', createSupplier);
+// router.post('/suppliers/:id', updateSupplier);
+// router.post('/suppliers/:nama', deleteSupplier);
+
+// export default router;
