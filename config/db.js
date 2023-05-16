b31a78cfeb4dae56018d91e3c92e2e50bb3c6d7d
fajarm <andhikadika7878@gmail.com>
2023-05-16 16:03:15 +0700

    update db.js

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ import mysql from 'mysql2';
 
 // Create a connection pool to the Cloud SQL database
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
+  host: '34.101.233.248',
+  user: 'fajar',
+  password: 'fajar',
   database: 'tcc',
 });
 
