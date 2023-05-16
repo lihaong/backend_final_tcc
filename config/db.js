29d369bcb7b3493fed26e62ddef6f01fd7f9f563
fajarm <andhikadika7878@gmail.com>
2023-05-16 16:16:50 +0700

    update db dan index

--- config/db.js
+++ config/db.js
@@ -1,11 +1,14 @@
 import mysql from 'mysql2';
 
+// Set the MySQL connection configuration
+const connectionConfig = {
+  host: process.env.DB_HOST || '34.101.233.248',
+  user: process.env.DB_USER || 'fajar',
+  password: process.env.DB_PASSWORD || 'fajar',
+  database: process.env.DB_NAME || 'tcc',
+};
+
 // Create a connection pool to the Cloud SQL database
-const pool = mysql.createPool({
-  host: '34.101.233.248',
-  user: 'fajar',
-  password: 'fajar',
-  database: 'tcc',
-});
+const pool = mysql.createPool(connectionConfig);
 
 export default pool;
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(userRoute);
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
